8a359e9a5222d740908d040e5668eda4eed53e00
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-11 16:43:05 -0400

    Homogenous coords are kinda awesome

--- geom.py
+++ geom.py
@@ -48,6 +48,14 @@ class Coord3(typing.NamedTuple):
     def dropTo2(self) -> Coord2:
         return Coord2(x=self.x, y=self.y)
 
+    def toH(self):
+        return Coord3H(
+            x=self.x,
+            y=self.y,
+            z=self.z,
+            w = 1,
+        )
+
 class Tri2(typing.NamedTuple):
     a: Coord2; b: Coord2; c: Coord2
 
@@ -84,5 +92,50 @@ class Hull(typing.NamedTuple):
 
 Geometry = list[Hull]
 
-class Xform3(object):
-    'TODO'
+class Coord3H(typing.NamedTuple):
+    x: float; y: float; z: float; w: float
+
+    def __add__(self, other):
+        if not isinstance(other, Coord3H):
+            return NotImplemented
+        return Coord3H(
+            x = self.x + other.x,
+            y = self.y + other.y,
+            z = self.z + other.z,
+            w = self.w + other.w,
+        )
+
+    def __rmul__(self, other):
+        if not isinstance(other, float):
+            return NotImplemented
+        return Coord3H(
+            x = self.x * other,
+            y = self.y * other,
+            z = self.z * other,
+            w = self.w * other,
+        )
+    
+    def pd(self):
+        return Coord3(
+            x = self.x / self.w,
+            y = self.y / self.w,
+            z = self.z / self.w,
+        )
+
+class Xform3(typing.NamedTuple):
+    # Columns
+    a: Coord3H; b: Coord3H; c: Coord3H; d: Coord3H
+
+    def applyH(self, c: Coord3H):
+        m = self
+        return ( 0.0
+            + (c.x * m.a)
+            + (c.y * m.b)
+            + (c.z * m.c)
+            + (c.w * m.d)
+        )
+    
+    def apply(self, c: Coord3):
+        return self.applyH(
+            c.toH()
+        ).pd()
