771b123bd3390f4cd423929928fe1771ff65091a
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-11 16:22:06 -0400

    Fix small typo

--- viewport.py
+++ viewport.py
@@ -11,12 +11,12 @@ class Viewport(object):
         self.focalLength = focalLength
         self.xform = xform
     
-    def map(self, vert: Coord3) -> Coord2:
+    def project(self, vert: Coord3) -> Coord2:
         v = self.xform.apply(vert)
         if v.z <= 0:
             return None
         fac = self.focalLength / v.z
         return fac * v.dropTo2()
     
-    def mapTri(self, tri: Tri3) -> Tri2:
+    def projectTri(self, tri: Tri3) -> Tri2:
         return tri.mapTo2(self.map)
