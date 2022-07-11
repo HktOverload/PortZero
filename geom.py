bcb3cb35e8c995f8841b0e1759e1ba60946d72f7
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-11 16:09:37 -0400

    Never mind

--- geom.py
+++ geom.py
@@ -8,16 +8,45 @@ Export(...) @ globals()
 class Coord2(typing.NamedTuple):
     x: float; y: float
 
+    def __add__(self, other):
+        if not isinstance(other, Coord2):
+            return NotImplemented
+        return Coord2(
+            x = self.x + other.x,
+            y = self.y + other.y,
+        )
+
     def __rmul__(self, other):
         if not isinstance(other, float):
             return NotImplemented
         return Coord2(
-            x = x * other,
-            y = y * other,
+            x = self.x * other,
+            y = self.y * other,
         )
 
 class Coord3(typing.NamedTuple):
     x: float; y: float; z: float
+
+    def __add__(self, other):
+        if not isinstance(other, Coord3):
+            return NotImplemented
+        return Coord3(
+            x = self.x + other.x,
+            y = self.y + other.y,
+            z = self.z + other.z,
+        )
+
+    def __rmul__(self, other):
+        if not isinstance(other, float):
+            return NotImplemented
+        return Coord3(
+            x = self.x * other,
+            y = self.y * other,
+            z = self.z * other,
+        )
+    
+    def dropTo2(self):
+        return Coord2(x=self.x, y=self.y)
 
 class Tri3(typing.NamedTuple):
     a: Coord3; b: Coord3; c: Coord3
