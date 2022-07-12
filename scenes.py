c0801ddaa45303ff4a89b108732811ff6b7bb7e2
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-12 17:10:52 -0400

    Need to work with multiple primitives

--- scenes.py
+++ scenes.py
@@ -1,11 +1,10 @@
 # PortZero by HktOverload
 
-import collections
+import collections, itertools
 from utils import *
 from events import *
 from entities import *
 from geom import *
-from worlds import *
 
 Export('Scene') @ globals()
 
@@ -16,7 +15,7 @@ class Scene(object):
     def __init__( self, name: str, entia: list[Ens]):
         self.name = name
         self.entia = entia
-        self.observers: t.Dict[str, t.List[Entity]] = {}
+        self.observers: t.Dict[str, t.Set[Entity]] = {}
         self.events: t.Deque[Event] = collections.deque()
         for i in range(len(entia)):
             self.updateObservers(i)
@@ -27,16 +26,37 @@ class Scene(object):
             if eventName not in self.observers:
                 self.observers[eventName] = []
             self.observers[eventName].append(entity)
+    
+    def removeObserver(self, entity: Entity):
+        for i in self.observers:
+            i.remove(entity)
 
     def sendSceneEvents(self) -> list[Event]:
-        ...
+        for entity, geometry in self.entia:
+            if entity in self.observers['!intersect']:
+                for otherEntity, other in self.entia:
+                    if intersection(geometry, other):
+                        self.events.append(
+                            Event(
+                                name = '!intersect',
+                                data = {
+                                    'other': otherEntity,
+                                }
+                            )
+                        )
+                        break
     
     def tick(self):
         self.sendSceneEvents()
+        for i in range(len(self.entia)):
+            self.entia[i][1] = None
         for event in self.events:
             if event.name == '!spawn':
                 self.entia.append((event.data['new-entity'], None))
                 self.updateObservers(len(self))
+            elif event.name == '!die':
+                self.entia.remove((event.data['target'], None))
+                self.removeObserver(event.data['target'])
             else:
                 for observer in self.observers[event.name]:
                     observer.recv(event)
