47f71a114672d0425cdb63ef43b41ed3dd59945b
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-12 15:14:59 -0400

    Forgot to call updateObservers

--- scenes.py
+++ scenes.py
@@ -13,14 +13,13 @@ Ens = tuple[Entity, Geometry]
 
 class Scene(object):
     __slots__ = 'name', 'entia', 'observers', 'events'
-    def __init__(
-        self, name: str,
-        entia: list[Ens], observers: dict[str, list[Entity]],
-    ):
+    def __init__( self, name: str, entia: list[Ens]):
         self.name = name
         self.entia = entia
-        self.observers = observers
+        self.observers: t.Dict[str, t.List[Entity]] = {}
         self.events: t.Deque[Event] = collections.deque()
+        for i in range(len(entia)):
+            self.updateObservers(i)
 
     def updateObservers(self, i: int):
         entity, _ = self.entia[i][0]
@@ -28,13 +27,16 @@ class Scene(object):
             if eventName not in self.observers:
                 self.observers[eventName] = []
             self.observers[eventName].append(entity)
+
+    def sendSceneEvents(self) -> list[Event]:
+        ...
     
     def tick(self):
-        self.updateObservers()
         self.sendSceneEvents()
         for event in self.events:
             if event.name == '!spawn':
                 self.entia.append((event.data['new-entity'], None))
+                self.updateObservers(len(self))
             else:
                 for observer in self.observers[event.name]:
                     observer.recv(event)
